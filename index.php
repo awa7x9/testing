d01b36305eb7adc3e5b8a7ade3d40ea5095afe30
awa7x9 <awais.webdeveloper@gmail.com>
2021-04-19 14:22:42 +0300

    spaces

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
+<body>  
 <?php
 $servername = "91.238.160.184";
 $username = "ruffbout_awais";
