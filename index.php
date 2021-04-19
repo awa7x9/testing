c2e87b0d1e93a1d97f6385c73b33d97c8a459db7
awa7x9 <awais.webdeveloper@gmail.com>
2021-04-19 14:10:28 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$servername = "91.238.160.184";
+$username = "ruffbout_awais";
+$password = "awais@123M";
+$dbname = "ruffbout_testing";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+$sql = "INSERT INTO `user` (`name`, `email`, `mobile`) VALUES ('Hana', 'hana@yahoo.com', '000000')";
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+?> 
+</body>
+</html>
